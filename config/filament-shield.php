d4bc5314ba4450690e0aa34ca9f946b0b41ef2ae
Yester Selga <yesterselga@gmail.com>
2022-08-11 09:36:45 +0800

    shield update

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -1,72 +1,73 @@
 <?php
 
 return [
-    'shield_resource' => [
-        'slug' => 'shield/roles',
-        'navigation_sort' => -1,
-        'navigation_badge' => true
-    ],
+     'shield_resource' => [
+          'slug' => 'roles',
+          'navigation_sort' => 2,
+          'navigation_badge' => false,
+          'navigation_group' => true,
+     ],
 
-    'auth_provider_model' => [
-        'fqcn' => 'App\\Models\\User'
-    ],
+     'auth_provider_model' => [
+          'fqcn' => 'App\\Models\\User'
+     ],
 
-    'super_admin' => [
-        'enabled' => true,
-        'name'  => 'super_admin'
-    ],
+     'super_admin' => [
+          'enabled' => true,
+          'name'  => 'super_admin'
+     ],
 
-    'filament_user' => [
-        'enabled' => true,
-        'name' => 'filament_user'
-    ],
+     'filament_user' => [
+          'enabled' => true,
+          'name' => 'filament_user'
+     ],
 
-    'permission_prefixes' => [
-        'resource' => [
-            'view',
-            'view_any',
-            'create',
-            'update',
-            'restore',
-            'restore_any',
-            'replicate',
-            'reorder',
-            'delete',
-            'delete_any',
-            'force_delete',
-            'force_delete_any',
-        ],
+     'permission_prefixes' => [
+          'resource' => [
+               'view',
+               'view_any',
+               'create',
+               'update',
+               // 'restore',
+               // 'restore_any',
+               // 'replicate',
+               // 'reorder',
+               // 'delete',
+               // 'delete_any',
+               // 'force_delete',
+               // 'force_delete_any',
+          ],
 
-        'page' => 'page',
-        'widget' => 'widget',
-    ],
+          'page' => 'page',
+          'widget' => 'widget',
+     ],
 
-    'entities' => [
-        'pages' => true,
-        'widgets' => true,
-        'resources' => true,
-        'custom_permissions' => false,
-    ],
+     'entities' => [
+          'pages' => true,
+          'widgets' => true,
+          'resources' => true,
+          'custom_permissions' => false,
+     ],
 
-    'generator' => [
-        'option' => 'policies_and_permissions'
-    ],
+     'generator' => [
+          'option' => 'policies_and_permissions'
+     ],
 
-    'exclude' => [
-        'enabled' => true,
+     'exclude' => [
+          'enabled' => true,
 
-        'pages' => [
-            'Dashboard',
-        ],
+          'pages' => [
+               'Dashboard',
+          ],
 
-        'widgets' => [
-            'AccountWidget','FilamentInfoWidget',
-        ],
+          'widgets' => [
+               'AccountWidget', 'FilamentInfoWidget',
+          ],
 
-        'resources' => [],
-    ],
+          'resources' => [],
+     ],
 
-    'register_role_policy' => [
-        'enabled' => false
-    ],
+     'register_role_policy' => [
+          'enabled' => false
+     ],
 ];
